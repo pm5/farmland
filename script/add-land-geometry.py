b177e52b8c3b06f0323c69fa05dd9ed3b1a5fec3
Pomin Wu <pomin5@gmail.com>
2016-01-15 00:16:37 +0800

    fix geometry transform script

--- script/add-land-geometry.py
+++ script/add-land-geometry.py
@@ -10,13 +10,13 @@ import sys
 csvfile = open('data/sgw-epa.csv', 'r')
 outfile = open('data/sgw-epa-geom.csv', 'w')
 landreader = csv.DictReader(csvfile)
-landwriter = csv.DictWriter(outfile, landreader.fieldnames)
+landwriter = csv.DictWriter(outfile, landreader.fieldnames + ["GeoJSON", "WKT"])
 landwriter.writeheader()
 
 limit = 6000
 
 for cells in landreader:
-    query_url = 'http://twland.ronny.tw/index/search?lands[]=' + cells['縣市別'] + ',' + cells['場址地號'] + ',' + cells['號']
+    query_url = 'http://twland.ronny.tw/index/search?lands[]=' + cells['縣市別'] + ',' + cells['段名'] + ',' + cells['地號']
     r = requests.get(query_url)
     if len(r.json()['features']) > 0:
         cells['GeoJSON'] = json.dumps(r.json())
@@ -26,11 +26,12 @@ for cells in landreader:
                 'coordinates': r.json()['features'][0]['geometry']['coordinates'][0]
             })
         except Exception:
-            sys.stderr.write("\n")
-            print(query_url)
+            sys.stdout.write(query_url + "\n")
+            sys.stdout.flush()
     landwriter.writerow(cells)
     limit = limit - 1
     time.sleep(10)
     sys.stderr.write('.')
+    sys.stderr.flush()
     if limit < 0:
         break
